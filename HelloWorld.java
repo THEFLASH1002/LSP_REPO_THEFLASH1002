4f2bf28052a0e5b24366186de33bef1134bd8da3
THEFLASH1002 <kgregg1211@gmail.com>
2025-08-19 12:47:46 -0400

    push

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Name: Kennedy Gregg
+ */
+
+ package org.howard.edu.lsp.assignment1;
+
+ public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!!");
+    }
+ }
