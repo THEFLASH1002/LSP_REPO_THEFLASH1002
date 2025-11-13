79f9ad012e9a8c4391e03f41bba91897152ef5a2
THEFLASH1002 <197106814+THEFLASH10002@users.noreply.github.com>
2025-11-13 12:35:10 -0500

    Update HelloWorld.java

--- src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -2,7 +2,7 @@
  * Name: Kennedy Gregg
  */
 
- package src.org.howard.edu.lsp.assignment1;
+ package org.howard.edu.lsp.assignment1;
 
  public class HelloWorld {
     public static void main(String[] args){
